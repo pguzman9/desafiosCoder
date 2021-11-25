9b112de1f29b7baff94dbd0419310236128e8ef2
pguzman9 <72151319+pguzman9@users.noreply.github.com>
2021-11-24 22:57:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const Container = require('../desafio2/contenedor.js')
+
+const app = express()
+const file = new Container('./productos.json')
+file.init()
+
+const PORT = 8080
+
+const server = app.listen(PORT , ()=>{
+    try{
+        console.log(`Servidor escuchando en puerto ${PORT}`)
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+app.get('/productos',(req, res) =>{
+    try{
+        const productos = file.getAll()
+        res.send(`Productos disponibles: ${JSON.stringify(productos)}`)
+    }
+    catch(err){
+        res.send(`Error al buscar los productos: ${err}`)
+    }
+})
+app.get('/productoRandom',(req, res) =>{
+    const id = Math.floor(Math.random() * 3) + 1
+    try{   
+        const productoRandom = file.getByID(id)
+        res.send(`El producto con id ${id}, es ${JSON.stringify(productoRandom)}`)
+    }
+    catch(err){
+        res.send(`Ocurrio un error al intentar obtener el producto por id ${err}`)
+    }
+})
