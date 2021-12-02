8f531c1ef031c43000f3e1ce1b963112281c296e
pguzman9 <72151319+pguzman9@users.noreply.github.com>
2021-12-02 18:33:15 -0300

    update desafío3
    modificación en endpoint productoRandom
    modificación de respuesta del servidor en ambos endpoints

--- app.js
+++ app.js
@@ -18,17 +18,18 @@ const server = app.listen(PORT , ()=>{
 app.get('/productos',(req, res) =>{
     try{
         const productos = file.getAll()
-        res.send(`Productos disponibles: ${JSON.stringify(productos)}`)
+        res.send((productos))
     }
     catch(err){
         res.send(`Error al buscar los productos: ${err}`)
     }
 })
 app.get('/productoRandom',(req, res) =>{
-    const id = Math.floor(Math.random() * 3) + 1
+    const productos = file.getAll()
+    const id = Math.floor(Math.random() * productos.length) + 1
     try{   
         const productoRandom = file.getByID(id)
-        res.send(`El producto con id ${id}, es ${JSON.stringify(productoRandom)}`)
+        res.send((productoRandom))
     }
     catch(err){
         res.send(`Ocurrio un error al intentar obtener el producto por id ${err}`)
